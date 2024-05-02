212b8d7a2aa933b7e0249fb5b66ac929ab849420
mathis <37186532+maaaathis@users.noreply.github.com>
2024-05-02 07:54:21 +0000

    add git pull before pushing

--- entrypoint.sh
+++ entrypoint.sh
@@ -170,6 +170,10 @@ echo "[+] git diff-index:"
 # git diff-index : to avoid doing the git commit failing if there are no changes to be commit
 git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
+echo "[+] git pull:"
+# git pull : to avoid doing the git push failing if there are any new commits since local cloning of the repo
+git pull
+
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
 git push "$GIT_CMD_REPOSITORY" --set-upstream "$TARGET_BRANCH"
